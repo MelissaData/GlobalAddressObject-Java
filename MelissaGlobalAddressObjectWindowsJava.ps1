42c0b9cc4e931603e46f391ee97266f5cfbc7340
Melissa <ha@melissadata.com>
2023-08-29 20:28:20 -0700

    Commit Global Address Object sample code in Java

---
+++ MelissaGlobalAddressObjectWindowsJava.ps1
@@ -0,0 +1,285 @@
+# Name:    MelissaGlobalAddressObjectWindowsJava
+# Purpose: Use the Melissa Updater to make the MelissaAddressObjectWindowsJava code usable
+
+######################### Parameters ##########################
+
+param(
+  $addressLine1 = "",
+  $addressLine2 = "",
+  $addressLine3 = "",
+  $locality = "",
+  $administrativeArea = "",
+  $postalCode = "",
+  $country = "",
+  $license = '',
+  [switch]$quiet = $false
+)
+
+######################### Classes ##########################
+
+class FileConfig {
+  [string] $FileName;
+  [string] $ReleaseVersion;
+  [string] $OS;
+  [string] $Compiler;
+  [string] $Architecture;
+  [string] $Type;
+}
+
+class ManifestConfig {
+  [string] $ManifestName;
+  [string] $ReleaseVersion;
+}
+
+######################### Config ###########################
+
+$RELEASE_VERSION = '2023.Q3'
+
+# Uses the location of the .ps1 file 
+# Modify this if you want to use 
+$CurrentPath = $PSScriptRoot
+Set-Location $CurrentPath
+$ProjectPath = "$CurrentPath\MelissaGlobalAddressObjectWindowsJava"
+$DataPath = "$ProjectPath\Data"
+
+If (!(Test-Path $DataPath)) {
+  New-Item -Path $ProjectPath -Name 'Data' -ItemType "directory"
+}
+
+$DLLs = @(
+  [FileConfig]@{
+    FileName       = "mdAddr.dll";
+    ReleaseVersion = $RELEASE_VERSION;
+    OS             = "WINDOWS";
+    Compiler       = "DLL";
+    Architecture   = "64BIT";
+    Type           = "BINARY";
+  },
+  [FileConfig]@{
+    FileName       = "mdGeo.dll";
+    ReleaseVersion = $RELEASE_VERSION;
+    OS             = "WINDOWS";
+    Compiler       = "DLL";
+    Architecture   = "64BIT";
+    Type           = "BINARY";
+  },
+  [FileConfig]@{
+    FileName       = "mdGlobalAddr.dll";
+    ReleaseVersion = $RELEASE_VERSION;
+    OS             = "WINDOWS";
+    Compiler       = "DLL";
+    Architecture   = "64BIT";
+    Type           = "BINARY";
+  },
+  [FileConfig]@{
+    FileName       = "mdRightFielder.dll";
+    ReleaseVersion = $RELEASE_VERSION;
+    OS             = "WINDOWS";
+    Compiler       = "DLL";
+    Architecture   = "64BIT";
+    Type           = "BINARY";
+  }
+)
+
+$WrapperCom = @(
+  [FileConfig]@{
+    FileName       = "mdGlobalAddrJavaWrapper.dll";
+    ReleaseVersion = $RELEASE_VERSION;
+    OS             = "WINDOWS";
+    Compiler       = "JAVA";
+    Architecture   = "64BIT";
+    Type           = "INTERFACE";
+  },
+  [FileConfig]@{
+    FileName       = "mdGlobalAddr_JavaCode.zip";
+    ReleaseVersion = $RELEASE_VERSION;
+    OS             = "ANY";
+    Compiler       = "ANY";
+    Architecture   = "ANY";
+    Type           = "DATA";
+  }
+)
+
+
+$Manifests = @(
+  [ManifestConfig]@{
+    ManifestName   = "global_dq_data";
+    ReleaseVersion = $RELEASE_VERSION;
+  }
+)
+
+######################## Functions #########################
+
+function DownloadDataFiles([string] $license) {
+  $DataProg = 0
+  Write-Host "`n=============================== MELISSA UPDATER ============================="
+  Write-Host "MELISSA UPDATER IS DOWNLOADING DATA FILE(S)..."
+
+  foreach ($Manifest in $Manifests) {
+    Write-Progress -Activity "Downloading Manifest(s)" -Status "$([math]::round($DataProg / $Manifests.Count * 100, 2))% Complete:"  -PercentComplete ($DataProg / $Manifests.Count * 100)
+
+    .\MelissaUpdater\MelissaUpdater.exe manifest -p $Manifest.ManifestName -r $Manifest.ReleaseVersion -l $license -t $DataPath 
+    if ($? -eq $False ) {
+      Write-Host "`nCannot run Melissa Updater. Please check your license string!"
+      Exit
+    } 	    
+  }
+
+  Write-Host "Melissa Updater finished downloading data file(s)!"
+}
+
+function DownloadDLLs() {
+  Write-Host "MELISSA UPDATER IS DOWNLOADING DLL(s)..."
+  $DLLProg = 0
+  foreach ($DLL in $DLLs) {
+    Write-Progress -Activity "Downloading DLL(s)" -Status "$([math]::round($DLLProg / $DLLs.Count * 100, 2))% Complete:"  -PercentComplete ($DLLProg / $DLLs.Count * 100)
+
+    # Check for quiet mode
+    if ($quiet) {
+      .\MelissaUpdater\MelissaUpdater.exe file --filename $DLL.FileName --release_version $DLL.ReleaseVersion --license $LICENSE --os $DLL.OS --compiler $DLL.Compiler --architecture $DLL.Architecture --type $DLL.Type --target_directory $ProjectPath > $null
+      if (($?) -eq $False) {
+        Write-Host "`nCannot run Melissa Updater. Please check your license string!"
+        Exit
+      }
+    }
+    else {
+      .\MelissaUpdater\MelissaUpdater.exe file --filename $DLL.FileName --release_version $DLL.ReleaseVersion --license $LICENSE --os $DLL.OS --compiler $DLL.Compiler --architecture $DLL.Architecture --type $DLL.Type --target_directory $ProjectPath 
+      if (($?) -eq $False) {
+        Write-Host "`nCannot run Melissa Updater. Please check your license string!"
+        Exit
+      }
+    }
+    
+    Write-Host "Melissa Updater finished downloading " $DLL.FileName "!"
+    $DLLProg++
+  }
+}
+
+function DownloadWrappers() {
+  
+  foreach ($File in $WrapperCom) {
+    # Check for quiet mode
+    if ($quiet) {
+      .\MelissaUpdater\MelissaUpdater.exe file --filename $File.FileName --release_version $File.ReleaseVersion --license $LICENSE --os $File.OS --compiler $File.Compiler --architecture $File.Architecture --type $File.Type --target_directory $ProjectPath > $null
+      if (($?) -eq $False) {
+        Write-Host "`nCannot run Melissa Updater. Please check your license string!"
+        Exit
+      }
+    }
+    else {
+      .\MelissaUpdater\MelissaUpdater.exe file --filename $File.FileName --release_version $File.ReleaseVersion --license $LICENSE --os $File.OS --compiler $File.Compiler --architecture $File.Architecture --type $File.Type --target_directory $ProjectPath 
+      if (($?) -eq $False) {
+        Write-Host "`nCannot run Melissa Updater. Please check your license string!"
+        Exit
+      }
+    }
+      
+    Write-Host "Melissa Updater finished downloading " $File.FileName "!"
+
+    # Check for the zip folder and extract from the zip folder if it was downloaded
+    if ($File.FileName -eq "mdGlobalAddr_JavaCode.zip") {
+      if (!(Test-Path ("$ProjectPath\mdGlobalAddr_JavaCode.zip"))) {
+        Write-Host "mdGlobalAddr_JavaCode.zip not found." 
+        
+        Write-Host "`nAborting program, see above.  Press any button to exit."
+        $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+        exit
+      }
+      else {
+        if (!(Test-Path ("$ProjectPath/com"))) {
+          Expand-Archive -Path "$ProjectPath\mdGlobalAddr_JavaCode.zip" -DestinationPath $ProjectPath
+        }
+        else {
+          # Remove the com folder before extracting
+          Remove-Item -Path "$ProjectPath/com" -Recurse -Force
+
+          Expand-Archive -Path "$ProjectPath\mdGlobalAddr_JavaCode.zip" -DestinationPath $ProjectPath
+        }
+      }
+    }
+  }
+}
+
+
+function CheckDLLs() {
+  Write-Host "`nDouble checking dll(s) were downloaded...`n"
+  $FileMissing = $false 
+  if (!(Test-Path ("$ProjectPath\mdAddr.dll"))) {
+    Write-Host "mdAddr.dll not found." 
+    $FileMissing = $true
+  }
+  if (!(Test-Path ("$ProjectPath\mdGeo.dll"))) {
+    Write-Host "mdGeo.dll not found." 
+    $FileMissing = $true
+  }
+  if (!(Test-Path ("$ProjectPath\mdGlobalAddr.dll"))) {
+    Write-Host "mdGlobalAddr.dll not found." 
+    $FileMissing = $true
+  }
+  if (!(Test-Path ("$ProjectPath\mdRightFielder.dll"))) {
+    Write-Host "mdRightFielder.dll not found." 
+    $FileMissing = $true
+  }
+  if ($FileMissing) {
+    Write-Host "`nMissing the above data file(s).  Please check that your license string and directory are correct."
+    return $false
+  }
+  else {
+    return $true
+  }
+}
+
+########################## Main ############################
+Write-Host "`n======================= Melissa Global Address Object =======================`n                    	[ Java | Windows | 64BIT ]`n"
+
+# Get license (either from parameters or user input)
+if ([string]::IsNullOrEmpty($license) ) {
+  $License = Read-Host "Please enter your license string"
+}
+
+# Check for License from Environment Variables 
+if ([string]::IsNullOrEmpty($License) ) {
+  $License = $env:MD_LICENSE # Get-ChildItem -Path Env:\MD_LICENSE   #[System.Environment]::GetEnvironmentVariable('MD_LICENSE')
+}
+
+if ([string]::IsNullOrEmpty($License)) {
+  Write-Host "`nLicense String is invalid!"
+  Exit
+}
+# Use Melissa Updater to download data file(s) 
+# Download data file(s) 
+DownloadDataFiles -license $License      # comment out this line if using DQS Release
+
+# Set data file(s) path
+# $DataPath = "C:\Program Files\Melissa DATA\DQT\Data"      # uncomment this line and change to your DQS Release data file(s) directory 
+
+# Download dll(s)
+DownloadDlls -license $License
+
+# Download wrapper and com folder
+DownloadWrappers -license $License
+
+# Check if all dll(s) have been downloaded. Exit script if missing
+$DLLsAreDownloaded = CheckDLLs
+if (!$DLLsAreDownloaded) {
+  Write-Host "`nAborting program, see above.  Press any button to exit."
+  $Host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+  exit
+}
+
+Write-Host "All file(s) have been downloaded/updated! "
+
+Set-Location $ProjectPath
+
+javac MelissaGlobalAddressObjectWindowsJava.java
+jar cvfm MelissaGlobalAddressObjectWindowsJava.jar manifest.txt *.class *.dll com\melissadata\*.class
+
+# Run Project
+if ([string]::IsNullOrEmpty($addressLine1) -and [string]::IsNullOrEmpty($addressLine2) -and [string]::IsNullOrEmpty($addressLine3) -and [string]::IsNullOrEmpty($locality) -and [string]::IsNullOrEmpty($administrativeArea) -and [string]::IsNullOrEmpty($postalCode) -and [string]::IsNullOrEmpty($country)) {
+  java -jar .\MelissaGlobalAddressObjectWindowsJava.jar --license $License --dataPath $DataPath
+}
+else {
+  java -jar .\MelissaGlobalAddressObjectWindowsJava.jar --license "$License" --dataPath "$DataPath" --addressLine1 "$addressLine1" --addressLine2 "$addressLine2" --addressLine3 "$addressLine3" --locality "$locality" --administrativeArea "$administrativeArea" --postalCode "$postalCode" --country "$country"
+}
+
+Set-Location ..
--- README.md
+++ README.md
@@ -1,3 +1,244 @@
-# Melissa - Global Address Object Windows Java 
+# Melissa GLobal Address Object Windows Java
 
-Coming soon
+
+## Purpose
+
+This code showcases the Melissa Global Address Object using Java.
+
+Please feel free to copy or embed this code to your own project. Happy coding!
+
+For the latest Melissa Global Address Object release notes, please visit: https://releasenotes.melissa.com/on-premise-api/global-address-object/
+
+For further details, please visit: https://wiki.melissadata.com/index.php?title=Global_Address_Object:Reference
+
+
+The console will ask the user for:
+
+
+- Address Line 1
+- Address Line 2
+- Address Line 3
+- Locality
+- Administrative Area
+- Postal Code
+- Country
+
+And return 
+
+- Melissa Address Key (MAK)
+- Company
+- Address Line 1
+- Address Line 2
+- Address Line 3
+- Address Line 4
+- Address Line 5
+- Locality
+- Administrative Area
+- Postal Code
+- Post Box
+- Country
+- Country ISO 2
+- Country ISO 3
+- Latitude
+- Longitude
+- Formatted Address
+- ResultCodes
+
+
+
+----------------------------------------
+
+## Tested Environments
+
+- Windows 64-bit Java 19
+- Powershell 5.1
+- Melissa data files for 2023-Q3
+
+----------------------------------------
+
+## Required Files and Programs
+
+#### Binaries
+
+This is the c++ code of the Melissa Object.
+
+- mdAddr.dll
+- mdGeo.dll
+- mdGlobalAddr.dll
+- mdRightFielder.dll
+
+
+
+#### Data File(s)
+- Addr.dbf
+- Congress.csv
+- dph256.dte
+- dph256.hsa
+- dph256.hsc
+- dph256.hsd
+- dph256.hsf
+- dph256.hsn
+- dph256.hsp
+- dph256.hsr
+- dph256.hst
+- dph256.hsu
+- dph256.hsv
+- dph256.hsx
+- dph256.hsy
+- dph256.hsz
+- ews.txt
+- icudt52l.dat
+- lcd256
+- mdAddr.dat
+- mdAddr.lic
+- mdAddr.nat
+- mdAddr.str
+- mdAddrKey.db
+- mdAddrKeyCA.db
+- mdCanada3.db
+- mdCanadaPOC.db
+- mdGeoCode.db3
+- mdGlobalAddr.ffbb
+- mdGlobalAddr.ffhb
+- mdGlobalAddr.ffpl
+- mdGlobalAddr.ffps
+- mdGlobalAddr.ffst
+- mdGlobalAddr.sac
+- mdLACS256.dat
+- mdRBDI.dat
+- mdRightFielder.cfg
+- mdRightFielder.dat
+- mdSteLink256.dat
+- mdSuiteFinder.db
+- month256.dat
+
+ 
+----------------------------------------
+## Getting Started
+These instructions will get you a copy of the project up and running on your local machine for development and testing purposes.
+This project is compatible with Java 19
+
+#### Install Java
+
+Before starting, make sure that Java has been correctly installed on your machine and your environment paths are configured. 
+
+You can download Java here: 
+https://www.oracle.com/java/technologies/downloads/
+
+You can check that your environment is set up correctly by opening a command prompt window and typing the following:
+`java --version`
+
+![alt text](/screenshots/java_version.PNG)
+
+If you see the version number then you have installed Java and set up your environment paths correctly!
+
+
+#### Set up Powershell settings
+
+If running Powershell for the first time, you will need to run this command in the Powershell console: `Set-ExecutionPolicy RemoteSigned`.
+The console will then prompt you with the following warning shown in the image below. 
+ - Enter `'A'`. 
+ 	- If successful, the console will not output any messages. (You may need to run Powershell as administrator to enforce this setting).
+	
+ ![alt text](/screenshots/powershell_executionpolicy.png)
+
+----------------------------------------
+
+#### Download this project
+```
+$ git clone https://github.com/MelissaData/GlobalAddressObject-Java
+$ cd GlobalAddressObject-Java
+```
+
+#### Set up Melissa Updater 
+
+Melissa Updater is a CLI application allowing the user to update their Melissa applications/data. 
+
+- Download Melissa Updater here: <https://releases.melissadata.net/Download/Library/WINDOWS/NET/ANY/latest/MelissaUpdater.exe>
+- Create a folder within the cloned repo called `MelissaUpdater`.
+- Put `MelissaUpdater.exe` in `MelissaUpdater` folder you just created.
+
+----------------------------------------
+
+#### Different ways to get data file(s)
+1.  Using Melissa Updater
+	- It will handle all of the data download/path and dll(s) for you.
+	- #### **Please be aware that this object will require about 100GB of memory** 
+2.  If you already have the latest GDQS Release (ZIP), you can find the data file(s) and dll(s) in there
+	- Use the location of where you copied/installed the data and update the "$DataPath" variable in the powershell script.
+	- Copy all the dll(s) mentioned above into the `MelissaAddressObjectWindowsJava` project folder.
+	
+## Run Powershell Script
+Please consult the release notes and wiki for the best way to input the parameters.
+
+Release Notes: https://releasenotes.melissa.com/on-premise-api/global-address-object/
+
+Wiki: https://wiki.melissadata.com/index.php?title=Result_Code_Details#Global_Address_Object
+
+
+Parameters:
+- -addressLine1: a test address line 1
+- -addressLine2 (optional): a test address line 2
+- -addressLine3 (optional): a test address line 3
+- -locality: a test locality
+- -administrativeArea: a test administrative area
+- -postalCode: a test postal code
+- -country: a test country
+ 	
+  These are convenient when you want to get results for a specific address in one run instead of testing multiple addresses in interactive mode.
+
+- -license (optional): a license string to test the Global Address Object
+- -quiet (optional): add to the command if you do not want to get any console output from the Melissa Updater
+
+When you have modified the script to match your data location, let's run the script. There are two modes:
+- Interactive 
+
+	The script will prompt the user for an address line 1, address line 2, address line 3, locality, administrative area, postal code, and country, then use the provided inputs to test Global Address Object. For example:
+	```
+	$ .\MelissaGlobalAddressObjectWindowsJava.ps1
+	```
+	For quiet mode:
+	```
+	$ .\MelissaGlobalAddressObjectWindowsJava.ps1 -quiet
+	```
+- Command Line 
+
+	You can pass an address line 1, address line 2, address line 3, locality, administrative area, postal code, country, and a license string into the ```-addressLine1```, ```-addressLine2```, ```-addressLine3```, ```-locality```, ```-administrativeArea```, ```-postalCode```, ```-country```, and ```-license``` parameters respectively to test Global Address Object. For example:
+
+	```
+	$ .\MelissaGlobalAddressObjectWindowsJava.ps1 -addressLine1 "Melissa Data GmbH" -addressLine2 "Cäcilienstr. 42-44" -addressLine3 "50667 Köln" -country "Germany" 
+	$ .\MelissaGlobalAddressObjectWindowsJava.ps1 -addressLine1 "Melissa Data GmbH" -addressLine2 "Cäcilienstr. 42-44" -addressLine3 "50667 Köln" -country "Germany" -license "<your_license_string>"
+	```
+
+	For quiet mode:
+	```
+	$ .\MelissaGlobalGlobalAddressObjectWindowsJava.ps1 -addressLine1 "Melissa Data GmbH" -addressLine2 "Cäcilienstr. 42-44" -addressLine3 "50667 Köln" -country "Germany" -quiet
+	$ .\MelissaGlobalGlobalAddressObjectWindowsJava.ps1 -addressLine1 "Melissa Data GmbH" -addressLine2 "Cäcilienstr. 42-44" -addressLine3 "50667 Köln" -country "Germany" -license "<your_license_string>" -quiet
+	```
+
+This is the expected output from a successful setup for interactive mode:
+
+![alt text](/screenshots/output.png)
+
+    
+## Troubleshooting
+
+Troubleshooting for errors found while running your program.
+
+### Errors:
+
+| Error      | Description |
+| ----------- | ----------- |
+| ErrorRequiredFileNotFound      | Program is missing a required file. Please check your Data folder and refer to the list of required files above. If you are unable to obtain all required files through the Melissa Updater, please contact technical support below. |
+| ErrorDatabaseExpired   | .db file(s) are expired. Please make sure you are downloading and using the latest release version. (If using the Melissa Updater, check powershell script for '$RELEASE_VERSION = {version}'  and change the release version if you are using an out of date release).     |
+| ErrorFoundOldFile   | File(s) are out of date. Please make sure you are downloading and using the latest release version. (If using the Melissa Updater, check powershell script for '$RELEASE_VERSION = {version}'  and change the release version if you are using an out of date release).    |
+| ErrorLicenseExpired   | Expired license string. Please contact technical support below. |
+
+
+## Contact Us
+
+For free technical support, please call us at 800-MELISSA ext. 4
+(800-635-4772 ext. 4) or email us at tech@melissa.com.
+
+To purchase this product, contact Melissa sales department at
+800-MELISSA ext. 3 (800-635-4772 ext. 3).
